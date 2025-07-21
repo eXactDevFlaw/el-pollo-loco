02f726fecc85752b231ec6712b4b43c4956f983d
Lutz Bölling <lutzboelling@gmx.de>
2025-07-21 11:30:33 +0200

    feat: added basic image loadout

--- classes/world.class.js
+++ classes/world.class.js
@@ -5,11 +5,22 @@ class World {
         new Chicken(),
         new Chicken(),
     ]
+    clouds = [
+        new Cloud(),
+    ]
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/air.png', 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
+    ]
+    canvas;
     ctx;
 
 
-    constructor(canvas){
+    constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas
         this.draw();
     }
 
@@ -21,6 +32,28 @@ class World {
 
 
     draw() {
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width)
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+        this.addObjecctToMap(this.backgroundObjects);
+        this.addToMap(this.character)
+
+        this.addObjecctToMap(this.enemies);
+        this.addObjecctToMap(this.clouds);
+
+
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
+    }
+
+    addObjecctToMap(objects) {
+        objects.forEach((o) => {
+            this.addToMap(o)
+        })
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height)
     }
 }
