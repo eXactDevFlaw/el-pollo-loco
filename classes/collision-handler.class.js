f484484296e5019199f5a7c95b21ee4214899892
Lutz Bölling <lutzboelling@gmx.de>
2025-07-31 22:59:16 +0200

    feat: added collision-hanlder to restructer logic of code an keep limit of 400loc, doc: added JSDoc to files

---
+++ classes/collision-handler.class.js
@@ -0,0 +1,139 @@
+/**
+ * Handles all collision detection and item collection in the game.
+ * Manages enemy damage, coin collection, and bottle pickup mechanics.
+ */
+class CollisionHandler {
+    /**
+    * Creates a new CollisionHandler.
+    * @param {World} world - Reference to the main game world
+    */
+    constructor(world) {
+        this.world = world;
+    }
+
+    /**
+    * Main collision check method that runs all collision systems.
+    * Called every frame from the world's game loop.
+    */
+    checkAllCollisions() {
+        this.checkEnemyCollisions();
+        this.checkCoinCollisions();
+        this.checkBottlePickup();
+    }
+
+    /**
+    * Checks collisions between character and all enemies.
+    * Triggers damage when character touches any enemy.
+    */
+    checkEnemyCollisions() {
+        this.world.level.enemies.forEach((enemy) => {
+            if (this.world.character.isColliding(enemy)) {
+                this.handlePlayerDamage(enemy);
+            }
+        });
+    }
+
+    /**
+     * Handles the logic when the player takes damage.
+     * @param {MovableObject} enemy
+     */
+    handlePlayerDamage(enemy) {
+        this.world.character.hit();
+        this.world.statusBar.setPercentage(this.world.character.energy);
+    }
+
+    /**
+     * Handles coin collection.
+     */
+    checkCoinCollisions() {
+        const coinsToRemove = [];
+
+        this.findCollidingCoins(coinsToRemove);
+        this.removeCollectedCoins(coinsToRemove);
+    }
+
+    /**
+     * Finds coins that collide with character and adds them to removal list.
+     * @param {number[]} coinsToRemove - Array to store coin indices
+     */
+    findCollidingCoins(coinsToRemove) {
+        this.world.level.coins.forEach((coin, i) => {
+            if (this.world.character.isColliding(coin)) {
+                coinsToRemove.push(i);
+                this.increaseCoinAmount();
+            }
+        });
+    }
+
+    /**
+     * Increases coin amount and updates status bar.
+     */
+    increaseCoinAmount() {
+        this.world.coinAmount++;
+        this.world.coinStatusBar.setPercentage(this.world.coinAmount);
+    }
+
+    /**
+     * Removes collected coins from the level.
+     * @param {number[]} coinsToRemove - Array of coin indices to remove
+     */
+    removeCollectedCoins(coinsToRemove) {
+        for (let i = coinsToRemove.length - 1; i >= 0; i--) {
+            this.world.level.coins.splice(coinsToRemove[i], 1);
+        }
+    }
+
+    /**
+     * Handles bottle pickup.
+     */
+    checkBottlePickup() {
+        if (this.canPickupMoreBottles()) {
+            const bottlesToRemove = [];
+            this.findCollidingBottles(bottlesToRemove);
+            this.removeCollectedBottles(bottlesToRemove);
+        }
+    }
+
+    /**
+     * Checks if player can pick up more bottles.
+     * @returns {boolean} True if bottle amount is less than maximum
+     */
+    canPickupMoreBottles() {
+        return this.world.bottleAmount < 5;
+    }
+
+    /**
+     * Finds bottles that collide with character and adds them to removal list.
+     * @param {number[]} bottlesToRemove - Array to store bottle indices
+     */
+    findCollidingBottles(bottlesToRemove) {
+        this.world.level.bottles.forEach((bottle, i) => {
+            if (this.world.character.isColliding(bottle) && this.world.bottleAmount < 5) {
+                bottlesToRemove.push(i);
+                this.increaseBottleAmount();
+            }
+        });
+    }
+
+    /**
+     * Increases bottle amount and updates status bar.
+     */
+    increaseBottleAmount() {
+        this.world.bottleAmount++;
+        if (this.world.bottleAmount > 5) {
+            this.world.bottleAmount = 5;
+        }
+        const percentage = (this.world.bottleAmount / 5) * 100;
+        this.world.bottleStatusBar.setPercentage(percentage);
+    }
+
+    /**
+     * Removes collected bottles from the level.
+     * @param {number[]} bottlesToRemove - Array of bottle indices to remove
+     */
+    removeCollectedBottles(bottlesToRemove) {
+        for (let i = bottlesToRemove.length - 1; i >= 0; i--) {
+            this.world.level.bottles.splice(bottlesToRemove[i], 1);
+        }
+    }
+}
--- classes/world.class.js
+++ classes/world.class.js
@@ -28,6 +28,7 @@ class World {
         this.level = level;
         this.lastThrowTime = 0;
         this.throwCooldown = 300;
+        this.collisionHandler = new CollisionHandler(this);
         this.draw();
         this.setWorld();
         this.run();
@@ -45,9 +46,7 @@ class World {
      */
     run() {
         setInterval(() => {
-            this.checkCollision();
-            this.checkCoinCollisions();
-            this.checkBottlePickup();
+            this.collisionHandler.checkAllCollisions();
             this.checkThrowObjects();
         }, 200);
     }
@@ -100,121 +99,6 @@ class World {
         this.bottleAmount--;
         const percentage = (this.bottleAmount / 5) * 100;
         this.bottleStatusBar.setPercentage(percentage);
-    }
-
-    /**
-     * Checks collisions between the character and all enemies.
-     */
-    checkCollision() {
-        this.level.enemies.forEach((enemy) => {
-            if (this.character.isColliding(enemy)) {
-                this.handlePlayerDamage(enemy);
-            }
-        });
-    }
-
-    /**
-     * Handles the logic when the player takes damage.
-     * @param {MovableObject} enemy
-     */
-    handlePlayerDamage(enemy) {
-        this.character.hit();
-        this.statusBar.setPercentage(this.character.energy);
-    }
-
-    /**
-     * Handles coin collection.
-     */
-    checkCoinCollisions() {
-        const coinsToRemove = [];
-
-        this.findCollidingCoins(coinsToRemove);
-        this.removeCollectedCoins(coinsToRemove);
-    }
-
-    /**
-     * Finds coins that collide with character and adds them to removal list.
-     * @param {number[]} coinsToRemove - Array to store coin indices
-     */
-    findCollidingCoins(coinsToRemove) {
-        this.level.coins.forEach((coin, i) => {
-            if (this.character.isColliding(coin)) {
-                coinsToRemove.push(i);
-                this.increaseCoinAmount();
-            }
-        });
-    }
-
-    /**
-     * Increases coin amount and updates status bar.
-     */
-    increaseCoinAmount() {
-        this.coinAmount++;
-        this.coinStatusBar.setPercentage(this.coinAmount);
-    }
-
-    /**
-     * Removes collected coins from the level.
-     * @param {number[]} coinsToRemove - Array of coin indices to remove
-     */
-    removeCollectedCoins(coinsToRemove) {
-        for (let i = coinsToRemove.length - 1; i >= 0; i--) {
-            this.level.coins.splice(coinsToRemove[i], 1);
-        }
-    }
-
-    /**
-     * Handles bottle pickup.
-     */
-    checkBottlePickup() {
-        if (this.canPickupMoreBottles()) {
-            const bottlesToRemove = [];
-            this.findCollidingBottles(bottlesToRemove);
-            this.removeCollectedBottles(bottlesToRemove);
-        }
-    }
-
-    /**
-     * Checks if player can pick up more bottles.
-     * @returns {boolean} True if bottle amount is less than maximum
-     */
-    canPickupMoreBottles() {
-        return this.bottleAmount < 5;
-    }
-
-    /**
-     * Finds bottles that collide with character and adds them to removal list.
-     * @param {number[]} bottlesToRemove - Array to store bottle indices
-     */
-    findCollidingBottles(bottlesToRemove) {
-        this.level.bottles.forEach((bottle, i) => {
-            if (this.character.isColliding(bottle) && this.bottleAmount < 5) {
-                bottlesToRemove.push(i);
-                this.increaseBottleAmount();
-            }
-        });
-    }
-
-    /**
-     * Increases bottle amount and updates status bar.
-     */
-    increaseBottleAmount() {
-        this.bottleAmount++;
-        if (this.bottleAmount > 5) {
-            this.bottleAmount = 5;
-        }
-        const percentage = (this.bottleAmount / 5) * 100;
-        this.bottleStatusBar.setPercentage(percentage);
-    }
-
-    /**
-     * Removes collected bottles from the level.
-     * @param {number[]} bottlesToRemove - Array of bottle indices to remove
-     */
-    removeCollectedBottles(bottlesToRemove) {
-        for (let i = bottlesToRemove.length - 1; i >= 0; i--) {
-            this.level.bottles.splice(bottlesToRemove[i], 1);
-        }
     }
 
     /**
