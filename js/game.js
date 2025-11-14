7bf061d9576e7bbf628188947ec857121bbe121a
eXactDevFlaw <lutzboelling@gmx.de>
2025-11-14 16:39:16 +0100

    feat: added init function

---
+++ js/game.js
@@ -0,0 +1,11 @@
+let canvas;
+let character = new Image();
+
+function init() {
+    canvas = document.getElementById('canvas');
+    character.src = '../img/2_character_pepe/2_walk/W-21.png'
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    init()
+})
